04193f13d013fabc2e22ae3acc1e2f05b1d03153
waitwait13 <karenhsu70645@gmail.com>
2025-09-17 11:34:23 +0800

    week02

---
+++ week02/week02-2b.cpp
@@ -0,0 +1,16 @@
+// week02-2b.cpp
+// SOIT106_ADVANCE_001 C++ string
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string a,ans;
+	cin >> a;
+	for(int i=a.length()-1; i>=0; i--){
+        ans += a[i];
+    }
+    cout << a << '+' << stoi(ans) << '=';
+    cout << stoi(a) + stoi(ans) << endl;
+} /// stoi() is C++11 (2011)
